20f2cd1ab146ac0e711ff9c36b30384211d9e34c
SiniJohanna <82939474+SiniJohanna@users.noreply.github.com>
2021-11-12 16:27:10 +0200

    toimii

--- App.js
+++ App.js
@@ -1,12 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import MapView, { Marker} from'react-native-maps';
+import { StyleSheet, TextInput, View, Button, Alert, Keyboard } from 'react-native';
+import * as Location from 'expo-location';
+import Constants from 'expo-constants';
 
 export default function App() {
+
+  const [address, setAddress] = useState('');
+  const [region, setRegion] = useState(
+    {latitude: 0,
+    longitude: 0}
+  );
+
+  
+  useEffect(() => {
+    (async() => {
+      let  { status} = await Location.requestForegroundPermissionsAsync();
+      if  (status!==   'granted') {
+        Alert.alert('No   permissionto get location')
+        return;
+      }
+      let  location= await Location.getCurrentPositionAsync({});
+      setRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0322,
+        longitudeDelta: 0.0221,
+      })
+      //console.log(location);
+    })();
+  }, []);
+
+  
+  
+
+  const getCoordinates = address => {
+    const KEY = process.env.EXPO_MAPQUEST_API_KEY || Constants.manifest.extra.apiKey;
+    console.log(address);
+    fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=${KEY}&location=${address}`)
+    .then(response=>  response.json())
+    .then(responseJson=>{ 
+      setRegion(
+        {
+          latitude: responseJson.results[0].locations[0].displayLatLng.lat,
+          longitude: responseJson.results[0].locations[0].displayLatLng.lng,
+          latitudeDelta:0.0322,
+          longitudeDelta:0.0221,
+        }
+      )
+    })
+    .catch(error=> { Alert.alert('Error',error); });
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <MapView
+      style={styles.map}
+      region={region}
+        >
+        <Marker coordinate={region}/>
+        </MapView>
+        <View style={{flex: 1}}>
+        <TextInput 
+        style={styles.input}
+        onChangeText={address=> setAddress(address)}
+        value={address} />
+        <Button onPress={() => getCoordinates(address)} title='Show' />
+      </View>
     </View>
   );
 }
@@ -18,4 +78,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  input: {
+    width:200,
+    borderColor:'gray',
+    borderWidth:1
+  },
+  map: {
+    flex:3,
+    width: "100%",
+    height: "100%"
+  }
+
+  
 });
+
+
+
+/*
+*/
